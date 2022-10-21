080934d3b86051d3bb77b012b8b96144586ea0a2
Vadim Sosnovsky <vadim73rus1234567@mail.ru>
2022-10-21 14:33:13 +0400

    Initial Commit

---
+++ Imagery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imagery
+//
+//  Created by Вадим Сосновский on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
